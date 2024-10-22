40b56560951983a743ac3d5e43a48e551e649441
blancmx <ee2278879330@163.com>
2024-10-22 21:17:24 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+
+	/*for (int j=1;j<=9;j++)
+	{
+		for (int i=1;i<=j;i++)
+		{
+			int c = i * j;
+			printf("%d * %d = %d\t",i,j,c);
+		}
+		printf("\n");
+	}*/
+	/*printf("name\tage\tgender\thobby\n");
+	printf("kunkun\t2.5\t女\t篮球\n");*/
+	int i;	
+	int countA = 0;
+	printf("请输入一个正整数");
+	scanf("%d",&i);
+	for (int a=2;a<=i;a++)
+	{
+		int count = 0;
+		
+		for (int b=2;b<a;b++)
+		{
+			
+			if (a%b==0)
+			{
+				count++;
+				break;
+			}
+		}
+			if (count==0)
+			{
+				countA++;
+			}
+	}
+	
+	printf("从2到%d的质数有%d个\n",i,countA);
+
+	return 0;
+}
